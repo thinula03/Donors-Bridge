badb12420201c30115edb5c98aae020774cc3c33
Thinula Damsith <107210849+thinula03@users.noreply.github.com>
2025-04-07 07:32:50 +0530

    React update for front

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}/{os.getenv('MYSQL_DB')}"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
